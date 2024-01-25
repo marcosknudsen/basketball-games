a449dadf7730a67e068f88392af91031ed2f9872
Marcos Knudsen <marcosknu14@gmail.com>
2024-01-25 08:51:18 -0300

    order leagues

--- src/components/ListadoLigas.jsx
+++ src/components/ListadoLigas.jsx
@@ -1,11 +1,13 @@
 import League from "./League";
+import order from "../../order.json"
 export default function ListadoLigas({ matches }) {
-
   return (
     <div>
-      {[...new Set(matches.map((m) => m.league.id))].map((l) => (
-        <League matches={matches.filter((m) => m.league.id == l)} key={l}/>
-      ))}
+      {[...new Set(matches.map((m) => m.league.id))]
+        .sort((a, b) => order.order.indexOf(a)-order.order.indexOf(b))
+        .map((l) => (
+          <League matches={matches.filter((m) => m.league.id == l)} key={l} />
+        ))}
     </div>
   );
 }
