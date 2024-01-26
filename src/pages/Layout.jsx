b62dc216fb18077989411b9ef16aa372e50cb34a
Marcos Knudsen <marcosknu14@gmail.com>
2024-01-26 15:30:33 -0300

    uppercase title

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
     <div className="bg-gray-400 min-h-screen flex flex-col items-center">
       <div className="flex items-center gap-5">
         <img src="https://cdn-icons-png.flaticon.com/512/889/889455.png" alt="basketball-icon" className="w-20" />
-        <p className="text-5xl">Promiedos Basketball</p>
+        <p className="text-5xl uppercase font-bold">Promiedos Basketball</p>
       </div>
       <Outlet />
     </div>
