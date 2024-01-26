251f7c8f165bda1c5a7734f12ae391267cf66ad5
Marcos Knudsen <marcosknu14@gmail.com>
2024-01-26 00:45:48 -0300

    add league flag

--- src/components/League.jsx
+++ src/components/League.jsx
@@ -3,8 +3,14 @@ import Match from "./Match";
 export default function League({ matches }) {
   return (
     <div className="m-3 text-center text-2xl">
-      <div className=" text-yellow-400 rounded-t-md bg-green-800">
+      <div className=" text-yellow-400 rounded-t-md bg-green-800 flex items-center justify-center gap-5">
+        {matches[0].country.code != " " && matches[0].country.flag && (
+          <img src={matches[0].country.flag ?? "null"} className="w-10" />
+        )}
         {matches[0].league.name}
+        {matches[0].country.code != " " && matches[0].country.flag && (
+          <img src={matches[0].country.flag ?? "null"} className="w-10" />
+        )}
       </div>
       <div className="text-white rounded-b-md bg-gray-600">
         {matches.map((m) => (
