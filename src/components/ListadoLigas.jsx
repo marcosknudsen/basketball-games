f91e319208601273c0c503120e0cde1205370d32
Marcos Knudsen <marcosknu14@gmail.com>
2024-01-26 00:28:48 -0300

    order fix

--- src/components/ListadoLigas.jsx
+++ src/components/ListadoLigas.jsx
@@ -4,10 +4,15 @@ export default function ListadoLigas({ matches }) {
   return (
     <div className="w-3/4 min-w-[600px] max-w-[800px]">
       {[...new Set(matches.map((m) => m.league.id))]
-        .sort((a, b) => order.order.indexOf(a) - order.order.indexOf(b))
+        .sort((a, b) => getOrder(a) - getOrder(b))
         .map((l) => (
           <League matches={matches.filter((m) => m.league.id == l)} key={l} />
         ))}
     </div>
   );
+}
+
+function getOrder(id){
+  const value=order.order.indexOf(id)
+  return value!=-1?value:9999
 }
