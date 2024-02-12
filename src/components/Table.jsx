b8794a42e75dcbc28ca860b7b61884777df91834
Marcos Knudsen <marcosknu14@gmail.com>
2024-02-12 02:11:56 -0300

    link to team matches on standing

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Table({
   standing,
   className
 }) {
   return (
     <table
-      className={`text-base text-gray-200 text-center border ${className??""}`}
+      className={`text-base text-gray-200 text-center border ${className ?? ""}`}
     >
       <thead className="text-center bg-green-800 rounded-t-md">
         {standing[0].group.name}
       </thead>
       <tbody className="bg-green-600 rounded-b-md p-1">
         {standing.map((s) => (
-          <tr
-            key={s.team.id}
-            className={`flex border first-of-type:border-t-2`}
-          >
-            <td>{s.position}°</td>
-            <td className="justify flex justify-center">
-              <img
-                src={s.team.logo}
-              />
-            </td>
-            <td className="text-left">{`${s.team.name}`}</td>
-          </tr>
+          <Link to={`/team/${s.team.id}`} key={s.team.id}>
+            <tr
+              className={`flex border first-of-type:border-t-2`}
+            >
+              <td>{s.position}°</td>
+              <td className="justify flex justify-center">
+                <img
+                  src={s.team.logo}
+                />
+              </td>
+              <td className="text-left">{`${s.team.name}`}</td>
+            </tr>
+          </Link>
         ))}
       </tbody>
     </table>
