d1ead9c33d6e49c419b5d66a9776a6d0953da79c
Marcos Knudsen <marcosknu14@gmail.com>
2024-02-18 18:23:01 -0300

    fix week reverse

--- src/components/Playoffs.jsx
+++ src/components/Playoffs.jsx
@@ -5,9 +5,9 @@ export default function Playoffs({ matches }) {
 
   return (
     <div className="w-3/4 max-w-[800px] lg:w-full">
-      {[...new Set(matches.map((m) => m.week))]
+      {[...new Set(matches.map((m) => m.week))].reverse()
         .map((week) => (
-          <Week matches={matches.reverse().filter((m) => m.week == week)} key={week} />
+          <Week matches={matches.filter((m) => m.week == week)} key={week} />
         ))}
     </div>
   )
