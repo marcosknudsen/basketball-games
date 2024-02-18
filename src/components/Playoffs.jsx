ee9829312a3b0014a4bffdffac71c2fe02846dc9
Marcos Knudsen <marcosknu14@gmail.com>
2024-02-18 18:21:44 -0300

    reverse array

--- src/components/Playoffs.jsx
+++ src/components/Playoffs.jsx
@@ -7,7 +7,7 @@ export default function Playoffs({ matches }) {
     <div className="w-3/4 max-w-[800px] lg:w-full">
       {[...new Set(matches.map((m) => m.week))]
         .map((week) => (
-          <Week matches={matches.filter((m) => m.week == week)} key={week} />
+          <Week matches={matches.reverse().filter((m) => m.week == week)} key={week} />
         ))}
     </div>
   )
