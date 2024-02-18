a9267edb9371960caf4470f053d25ec47b054fec
Marcos Knudsen <marcosknu14@gmail.com>
2024-02-18 17:50:01 -0300

    get matches by league helper

---
+++ helpers/getMatchesByLeague.js
@@ -0,0 +1,19 @@
+import data from "../leagueMatches.json"
+
+export default async function (league) {
+  /* let response = await fetch(
+    `https://v1.basketball.api-sports.io/games?timezone=America/Argentina/Buenos_Aires&league=${league}&season=2023-2024`,
+    {
+      method: "GET",
+      headers: { "x-apisports-key": import.meta.env.VITE_TOKEN },
+    }
+  );
+  response = await response.json(); */
+  let response= data
+  response = response.response;
+  let a={
+    "type": "playoff",
+    "data": response,
+  }
+  return a;
+}
