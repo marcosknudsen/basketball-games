04757500f33c6465e6542c3e0255f8c1ac280fa4
Marcos Knudsen <marcosknu14@gmail.com>
2024-02-19 23:01:28 -0300

    add repag button

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { FaArrowUpLong } from "react-icons/fa6";
 
 export default function Layout() {
+  const [scrollPosition, setScrollPosition] = useState(0)
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position)
+  }
+
+  const handleClick = () => {
+    window.scrollTo(0, 0)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
   return (
     <div className="bg-cyan-700 min-h-screen flex flex-col items-center gap-5 pt-5">
+      {scrollPosition > 500 &&
+        <button onClick={handleClick} className="fixed right-16 bottom-10 py-4 px-8 rounded-lg text-2xl hover:bg-green-800 transition-all text-yellow-400 font-bold uppercase bg-green-700">
+          <FaArrowUpLong fontSize={40} />
+        </button>
+      }
       <div className="flex items-center justify-center gap-5 sm:gap-2 w-full">
         <img
           src="https://cdn-icons-png.flaticon.com/512/889/889455.png"
