f94c8e81a215f516969f8d19fc7162d53d97e91e
Marcos Knudsen <marcosknu14@gmail.com>
2024-05-15 00:40:03 -0300

    fix react import

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Index from "@/features/index/Index"
