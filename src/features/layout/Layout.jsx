a3fc15f1fc766e1edb3af325af8c2cdb530db66c
Marcos Knudsen <marcosknu14@gmail.com>
2024-05-15 00:39:16 -0300

    fix import

--- src/features/layout/Layout.jsx
+++ src/features/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Outlet,FaArrowUpLong } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { FaArrowUpLong } from "react-icons/fa6";
 
 export default function Layout() {
   const [scrollPosition, setScrollPosition] = useState(0)
