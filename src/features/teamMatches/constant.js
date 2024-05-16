f28e59dd8a97d3d64386032c95e42ee47385befc
Marcos Knudsen <marcosknu14@gmail.com>
2024-05-16 00:40:00 -0300

    teammatches constants file

---
+++ src/features/teamMatches/constant.js
@@ -0,0 +1,7 @@
+export const WIN_RESULT="W"
+export const LOSE_RESULT="L"
+export const NOT_PLAYED_RESULT="N"
+
+export const SHORT_CODE_FINISHED="FT"
+export const SHORT_AFTER_OVERTIME="AOT"
+export const SHORT_CODE_NOT_STARTED="NS"
