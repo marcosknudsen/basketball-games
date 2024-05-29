115e02a83115000fb47f99e5486c8af300c785aa
Marcos Knudsen <marcosknu14@gmail.com>
2024-05-29 01:49:40 -0300

    add base

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_API_BASE_URL,
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@components': '/src/components',
+      "@": "/src",
+      "@components": "/src/components",
     },
   },
   server: {
     proxy: {
-      '/api': {
+      "/api": {
         target: "https://api.b365api.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
+});
