1981de3aeb153fb17b6f119f96273059d3b0d878
Marcos Knudsen <marcosknu14@gmail.com>
2024-05-29 01:56:55 -0300

    remove base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_API_BASE_URL,
   plugins: [react()],
   resolve: {
     alias: {
