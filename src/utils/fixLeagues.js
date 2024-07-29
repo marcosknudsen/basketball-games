9eee5e450bca0c45995d4fd45808ef75b3ddc34d
Marcos Knudsen <marcosknu14@gmail.com>
2024-07-28 23:19:16 -0300

    add olympics logo

--- src/utils/fixLeagues.js
+++ src/utils/fixLeagues.js
@@ -1,9 +1,13 @@
+import OlympicsLogo from "@/images/league_logos/Olympics.png";
+
 const leaguesData={
     1304:{name:"Liga Nacional"},
     7592:{name:"Liga Argentina"},
     28862:{name:"Liga Federal"},
     1525:{name:"ACB"},
-    1583:{name:"Lega 1"}
+    1583:{name:"Lega 1"},
+    37671:{logo:OlympicsLogo},
+    37729:{logo:OlympicsLogo},
 }
 
 export default function(league){
