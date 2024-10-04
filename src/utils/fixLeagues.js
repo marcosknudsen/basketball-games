302814883330193d81cba89ec7bd2114131f80a0
Marcos Knudsen <marcosknu14@gmail.com>
2024-10-03 23:40:13 -0300

    add interligas

--- src/utils/fixLeagues.js
+++ src/utils/fixLeagues.js
@@ -1,23 +1,25 @@
 import OlympicsLogo from "@/images/league_logos/Olympics.png";
+import interligasLogo from "@/images/league_logos/Interligas.png";
 
-const leaguesData={
-    1304:{name:"Liga Nacional"},
-    7592:{name:"Liga Argentina"},
-    28862:{name:"Liga Federal"},
-    1525:{name:"ACB"},
-    1583:{name:"Lega 1"},
-    37671:{logo:OlympicsLogo,name:"Olympics 2024"},
-    37729:{logo:OlympicsLogo,name:"Olympics 2024 - Women"},
-    37733:{logo:OlympicsLogo,name:"Olympics 2024 - 3x3"},
-    37731:{logo:OlympicsLogo,name:"Olympics 2024 - 3x3 Women"},
-}
+const leaguesData = {
+  1304: { name: "Liga Nacional" },
+  7592: { name: "Liga Argentina" },
+  28862: { name: "Liga Federal" },
+  1525: { name: "ACB" },
+  1583: { name: "Lega 1" },
+  37671: { logo: OlympicsLogo, name: "Olympics 2024" },
+  37729: { logo: OlympicsLogo, name: "Olympics 2024 - Women" },
+  37733: { logo: OlympicsLogo, name: "Olympics 2024 - 3x3" },
+  37731: { logo: OlympicsLogo, name: "Olympics 2024 - 3x3 Women" },
+  34642: { logo: interligasLogo },
+};
 
-export default function(league){
-    let {name,logo}=leaguesData[league.id]??{}
-    if (name){
-        league.name=name
-    }
-    if (logo){
-        league.logo=logo
-    }
+export default function (league) {
+  let { name, logo } = leaguesData[league.id] ?? {};
+  if (name) {
+    league.name = name;
+  }
+  if (logo) {
+    league.logo = logo;
+  }
 }
