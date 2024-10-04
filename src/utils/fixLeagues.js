d5a5df1a7d1456e866d693220763de6b92aa7654
Marcos Knudsen <marcosknu14@gmail.com>
2024-10-04 00:51:21 -0300

    add logos

--- src/utils/fixLeagues.js
+++ src/utils/fixLeagues.js
@@ -1,5 +1,7 @@
-import OlympicsLogo from "@/images/league_logos/Olympics.png";
+import olympicsLogo from "@/images/league_logos/Olympics.png";
 import interligasLogo from "@/images/league_logos/Interligas.png";
+import euroLeagueLogo from "@/images/league_logos/EuroLeague.png";
+import clubsFriendlyLogo from "@/images/league_logos/ClubsFriendly.png";
 
 const leaguesData = {
   1304: { name: "Liga Nacional" },
@@ -7,11 +9,13 @@ const leaguesData = {
   28862: { name: "Liga Federal" },
   1525: { name: "ACB" },
   1583: { name: "Lega 1" },
-  37671: { logo: OlympicsLogo, name: "Olympics 2024" },
-  37729: { logo: OlympicsLogo, name: "Olympics 2024 - Women" },
-  37733: { logo: OlympicsLogo, name: "Olympics 2024 - 3x3" },
-  37731: { logo: OlympicsLogo, name: "Olympics 2024 - 3x3 Women" },
+  37671: { logo: olympicsLogo, name: "Olympics 2024" },
+  37729: { logo: olympicsLogo, name: "Olympics 2024 - Women" },
+  37733: { logo: olympicsLogo, name: "Olympics 2024 - 3x3" },
+  37731: { logo: olympicsLogo, name: "Olympics 2024 - 3x3 Women" },
   34642: { logo: interligasLogo },
+  1923: { logo: euroLeagueLogo},
+  14: { logo: clubsFriendlyLogo },
 };
 
 export default function (league) {
