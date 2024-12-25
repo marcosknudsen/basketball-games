4a9d76e06283ae61715dbdc2ad1c115cfa41f2b9
Marcos Knudsen <knudsenmarcos@gmail.com>
2024-12-25 20:16:54 -0300

    remove line

--- src/utils/fixLeaguesStanding.js
+++ src/utils/fixLeaguesStanding.js
@@ -2,7 +2,6 @@ const leaguesConfig = {
   2274: { standings: ["Western Conference", "Eastern Conference"] },
 };
 
-
 export default function fixLeaguesStanding(league_id) {
   return leaguesConfig[league_id] ? leaguesConfig[league_id].standings : [];
 }
