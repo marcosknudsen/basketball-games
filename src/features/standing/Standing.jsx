9ac972ddb1892f301d0f03fa39942f29d520ecc2
Marcos Knudsen <knudsenmarcos@gmail.com>
2024-12-26 00:06:40 -0300

    multiple tables

--- src/features/standing/Standing.jsx
+++ src/features/standing/Standing.jsx
@@ -1,7 +1,7 @@
 import { useLoaderData, useParams, useNavigate } from "react-router-dom";
-import TwoTables from "@components/TwoTables";
-import OneTable from "@components/OneTable";
 import standingDivisions from "@/settings/standingDivisions.json"
+import TablesContainer from "@/components/TablesContainer";
+import Table from "@/components/Table";
 
 export default function Standing() {
   const response = useLoaderData();
@@ -13,7 +13,7 @@ export default function Standing() {
 
   return (
     <>
-      {<p className="text-yellow-400 text-3xl font-bold uppercase">{season?.name??"La tabla seleccionada aún no está disponible"}</p>}
+      {<p className="text-yellow-400 text-3xl font-bold uppercase">{season?.name ?? "La tabla seleccionada aún no está disponible"}</p>}
       <div>
         <button
           className="bg-green-800 p-3 rounded-md text-yellow-400 w-28 uppercase font-semibold hover:bg-green-700 transition-colors"
@@ -23,17 +23,13 @@ export default function Standing() {
         </button>
       </div>
       <div className="min-h-[740px] flex gap-[150px] items-center w-full justify-center">
-        {
-          response ? (<>
-
-            {tables.length > 1 ? (
-              <>
-                <TwoTables standing1={tables[0]} standing2={tables[1]} qualifyArray={standingDivisions[leagueId]} leagueId={leagueId} />
-              </>
-            ) : (
-              <OneTable standing={tables[0]} qualifyArray={standingDivisions[leagueId]} leagueId={leagueId} />
-            )}
-          </>):null}
+        <TablesContainer>
+          {
+            tables.map((table) => 
+              <Table standing={table} qualifyArray={standingDivisions[leagueId]} leagueId={leagueId} className={`w-1/${tables.length}`} oneTable={tables.length === 1} />
+            )
+          }
+        </TablesContainer>
       </div>
     </>
   );
