ce3795811b3e3d523991cec6d6c4dc6a3b07ddac
marcosknudsen <marcosknu14@gmail.com>
2025-07-05 03:02:24 -0300

    Tomorrow

--- src/features/tomorrow/Tomorrow.jsx
+++ src/features/tomorrow/Tomorrow.jsx
@@ -1,20 +1,23 @@
-import { useLoaderData,useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import ListadoLigas from "@components/Results";
 import Maintenance from "../maintenance/Maintenance";
 import NoMatches from "../noMatches/NoMatches";
+import "../../styles/features/Tomorrow.css";
 
 export default function Tomorrow() {
   const matches = useLoaderData();
   const navigate = useNavigate();
+
   return (
     <>
-      <button
-        className="bg-green-800 p-3 rounded-md text-yellow-400 w-28 uppercase font-semibold hover:bg-green-700 transition-colors"
-        onClick={() => navigate("/")}
-      >
+      <button className="volver-button" onClick={() => navigate("/")}>
         Hoy
       </button>
-      {matches && matches.length ? <ListadoLigas matches={matches} /> : <NoMatches />}
+      {matches && matches.length ? (
+        <ListadoLigas matches={matches} />
+      ) : (
+        <NoMatches />
+      )}
     </>
   );
 }
