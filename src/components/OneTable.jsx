5fb8cd873a23435f44e49f94780565d634b503cf
marcosknudsen <marcosknu14@gmail.com>
2025-07-05 03:11:11 -0300

    OneTable

--- src/components/OneTable.jsx
+++ src/components/OneTable.jsx
@@ -1,5 +1,13 @@
 import Table from "@components/Table";
+import "../styles/components/OneTable.css";
 
-export default function OneTable({ standing,qualifyArray,leagueId }) {
-  return (<Table standing={standing} className="full" qualifyArray={qualifyArray} leagueId={leagueId}/>);
+export default function OneTable({ standing, qualifyArray, leagueId }) {
+  return (
+    <Table
+      standing={standing}
+      className="table-full"
+      qualifyArray={qualifyArray}
+      leagueId={leagueId}
+    />
+  );
 }
