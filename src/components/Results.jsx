69dd72000e591332e2f76faebd2724a7fb686189
marcosknudsen <marcosknu14@gmail.com>
2025-07-05 03:14:16 -0300

    Result

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,20 +1,25 @@
 import League from "@components/League";
 import leagues from "@/settings/leagues.json";
+import "../styles/components/Result.css";
 
 export default function Results({ matches }) {
-  console.log(matches)
   return (
-    <div className="w-3/4 max-w-[800px] lg:w-full">
+    <div className="results-container">
       {[...new Set(matches.map((m) => m.league.id))]
         .sort((a, b) => getOrder(a) - getOrder(b))
         .map((l) => (
-          <League matches={matches.filter((m) => m.league.id == l).sort((a, b) => a.date - b.date)} key={l} />
+          <League
+            matches={matches
+              .filter((m) => m.league.id == l)
+              .sort((a, b) => a.date - b.date)}
+            key={l}
+          />
         ))}
     </div>
   );
 }
 
 function getOrder(id) {
-  const value = leagues.order.indexOf(parseInt(id))
-  return value != -1 ? value : 9999
+  const value = leagues.order.indexOf(parseInt(id));
+  return value !== -1 ? value : 9999;
 }
