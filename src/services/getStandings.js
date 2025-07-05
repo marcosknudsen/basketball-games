2412ddf62c5db2027a4d6f3f15a9cf9f4f183187
marcosknudsen <marcosknu14@gmail.com>
2025-07-05 03:36:57 -0300

    get latest season

--- src/services/getStandings.js
+++ src/services/getStandings.js
@@ -7,32 +7,31 @@ export default async function getStandings(league) {
   let response = await fetch(
     `/api/v3/league/table?token=${import.meta.env.VITE_TOKEN}&sport_id=18&league_id=${league}`
   );
-  
+
   response = await response.json();
   response = response.results
   if (!response)
-    return {season: null, tables: []};
+    return { season: null, tables: [] };
   let season = getSeason(response)
   let tables = season.overall.tables
   logger(STANDINGS_LOG_STRING, league);
   if (!response || !response.length) {
     return null;
   }
-  
+
   let tablesNames = fixLeaguesStanding(league);
 
-  if (tablesNames.length){
-    tables = tables.filter(t=> tablesNames.includes(t.name));
+  if (tablesNames.length) {
+    tables = tables.filter(t => tablesNames.includes(t.name));
   }
 
   tables.map((table) => table.rows.map((pos) => fixClubs(pos.team)));
-  
+
   return { season: season.season, tables };
 }
 
 function getSeason(seasons) {
-  let date = Date.now()
-  let response = seasons.filter(s=> date >= new Date(parseInt(s.season.start_time)*1000) && date <= new Date(parseInt(s.season.end_time)*1000))[0]
-  
-  return response
+    return seasons.reduce((latest, current) => {
+        return Number(current.season.start_time) > Number(latest.season.start_time) ? current : latest;
+    }, seasons[0]);
 }
