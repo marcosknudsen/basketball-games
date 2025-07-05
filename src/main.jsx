0e88e7b3ba366587b20771f5c4fd57d77d3c4210
marcosknudsen <marcosknu14@gmail.com>
2025-07-04 23:15:50 -0300

    fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import Index from "@/features/index/Index"
-import todayLoader from "@/features/index/loader.js"
-import Tomorrow from "@/features/tomorrow/Tomorrow"
-import tomorrowLoader from "@/features/tomorrow/loader.js"
-import Yesterday from "@/features/yesterday/Yesterday"
-import yesterdayLoader from "@/features/yesterday/loader.js"
-import TeamMatches from "@/features/teamMatches/TeamMatches"
-import teamMatchesLoader from "@/features/teamMatches/loader.js"
-import Match from "@/features/match/Match"
-import gameStatsLoader from "@/features/match/loader.js"
+import Index from "./features/index/Index"
+import todayLoader from "./features/index/loader.js"
+import Tomorrow from "./features/tomorrow/Tomorrow"
+import tomorrowLoader from "./features/tomorrow/loader.js"
+import Yesterday from "./features/yesterday/Yesterday"
+import yesterdayLoader from "./features/yesterday/loader.js"
+import TeamMatches from "./features/teamMatches/TeamMatches"
+import teamMatchesLoader from "./features/teamMatches/loader.js"
+import Match from "./features/match/Match"
+import gameStatsLoader from "./features/match/loader.js"
 import Layout from "./features/layout/Layout";
-import "@/index.css";
-import "@/assets/Match.css";
-import "@/assets/Layout.css";
-import Standing from "@/features/standing/Standing"
+import "./index.css";
+import "./styles/features/Match.css";
+import "./styles/features/Layout.css";
+import Standing from "./features/standing/Standing"
 import { loader as standingLoader } from "@/features/standing/loader"
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
